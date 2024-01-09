05d8f4f03dd762243ea5102d94da679edcc82641
wild <wild.addsystem@gmail.com>
2024-01-09 16:45:44 -0400

    animacion
    animacion

--- js/java.js
+++ js/java.js
@@ -4,12 +4,21 @@ window.onload = function () {
 }
 
 // funcion abrirSobre
-function abrirSobre() {
-        // $('#sobreArriba').animate({
-        //         top: '-1000px'
-        // }, 1000, function () {
-        //         $('#modalSobre').modal('hide');
-        // });
 
-        $('#modalSobre').modal('hide');
+function abrirSobre() {
+
+        var sobreAriba = document.getElementById('sobreAriba');
+        var sobreAbajo = document.getElementsByClassName('sobreAbajo')[0]; // Selecciona la primera imagen con la clase 'sobreAbajo'
+
+        // Mover la imagen 'sobreAriba' hacia arriba
+        sobreAriba.style.transform = 'translateY(-450px)'; // Cambiar la posición vertical - ajusta este valor según lo necesites
+
+        // Mover la imagen 'sobreAbajo' hacia abajo
+        sobreAbajo.style.transform = 'translateY(450px)'; // Cambiar la posición vertical - ajusta este valor según lo necesites
+
+        // esperar la animación de las imágenes
+        setTimeout(function () {
+                $('#modalSobre').modal('hide');
+        }, 2000); // Tiempo de espera en milisegundos - ajusta este valor según lo necesites
+      
 }
